aa4792b4f811181ce9dcbec0086007ab1b252373
SmartCart2 <cougars1123@hotmail.com>
2026-08-25 19:14:59 -0400

    upgrade supabase package

--- edge-platform/src/lib/supabase.js
+++ edge-platform/src/lib/supabase.js
@@ -1,13 +1,7 @@
  
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
-  {
-    auth: {
-      autoRefreshToken: true,
-      persistSession: true
-    }
-  }
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
