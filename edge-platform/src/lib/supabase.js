5703b957ce9b45637113a2ff26d437d04bd9a660
SmartCart2 <cougars1123@hotmail.com>
2026-08-25 19:41:49 -0400

    Update supabase.js

--- edge-platform/src/lib/supabase.js
+++ edge-platform/src/lib/supabase.js
@@ -1,7 +1,7 @@
  
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  'https://azuymgyyjxusaicgrtxk.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6dXltZ3l5anh1c2FpY2dydHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2OTQyOTUsImV4cCI6MjEwMzI3MDI5NX0.xiAwXDGxexfLE1kJOo_tKizo4jhgTxxGc1F_X79UCho'
+);
